e2d8368600c950d1a7cd67a0c066bab0ae38da49
Pr1awrence <galabizkit@mail.ru>
2019-05-30 21:37:52 +0300

    Created 2 components - play field and cell. Added simple styles for them. In addition, the location of the cell is transmitted.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PlayFieldComponent } from './components/play-field/play-field.component';
+import { GameCellComponent } from './components/game-cell/game-cell.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayFieldComponent,
+    GameCellComponent
   ],
   imports: [
     BrowserModule
